b5442321eeae1412e14f7d1df828e066d2f72338
Tiffany Jianto <t.funnee@gmail.com>
2012-05-01 16:50:31 -0700

    TPCLogTest and TPCLog. LogTest not completely done

---
+++ src/edu/berkeley/cs162/TPCLogTest.java
@@ -0,0 +1,26 @@
+package edu.berkeley.cs162;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TPCLogTest {
+
+	//test that adds to disk successfully
+	public void testAppendAndFlush() {
+		
+	}
+	
+	//test that correctly rebuilds keyserver by executing each entry in the log
+	public void testRebuildKeyServer() {
+		
+	}
+	
+	/*
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+	*/
+
+}
