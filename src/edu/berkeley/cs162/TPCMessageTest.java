94cc30b313d426854abeb8e2e073c8c3299901d0
Jonathan Tien <jonathan.tien@gmail.com>
2012-05-02 11:32:32 -0700

    initial draft for tpcmessage unit tests

---
+++ src/edu/berkeley/cs162/TPCMessageTest.java
@@ -0,0 +1,17 @@
+package edu.berkeley.cs162;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class TPCMessageTest {
+
+	@Test
+	public void goodTPCMessage() {
+		
+	}
+	
+	@Test
+	public void badTPCMessage() {
+		
+	}
+}
